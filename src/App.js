935f14f43c5e137bca0e100f8a97be9c480b64f4
Mariusz <smajdormariusz98@gmail.com>
2022-03-18 15:45:11 +0100

    buttons component and dummy tasks array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import MainHeader from "./MainHeader";
+import MainHeader from "./MainHeader"
 import Container from "./Container"
 import Form from "./Form"
 import Section from "./Section"
+import Buttons from "./Buttons"
 
 function App() {
+  const DUMMY_TASKS = [
+    { content: "Lorem ipsum", done: false },
+    { content: "dolor sit amet", done: true }
+  ]
+
   return (
     <Container>
       <MainHeader title="Lista zadań" />
       <Section title="Dodaj nowe zadanie" body={<Form />} />
-      <Section title="Lista zadań" />
+      <Section title="Lista zadań" extraHeaderContent={<Buttons tasks={DUMMY_TASKS} />} />
     </Container>
   );
 }
---
+++ src/Buttons/index.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react"
+import "./style.css"
+
+const Buttons = props => {
+  return (
+    <div className="buttons">
+      {props.tasks.length > 0 && (
+        <Fragment>
+          <button className="buttons__button">
+            {props.hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </button>
+          <button className="buttons__button" disabled={props.tasks.every(task => task.done)} >
+            Ukończ wszystkie
+          </button>
+        </Fragment>
+      )}
+    </div>
+  )
+}
+
+export default Buttons
